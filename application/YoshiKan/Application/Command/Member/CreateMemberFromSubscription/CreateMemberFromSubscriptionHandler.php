3ee9fcc972f84d364f3dc7506abad9caa2fc21c1
Koen Caerels <koen.caerels@gmail.com>
2023-01-07 12:11:52 +0100

    Some licensing info.

--- application/YoshiKan/Application/Command/Member/CreateMemberFromSubscription/CreateMemberFromSubscriptionHandler.php
+++ application/YoshiKan/Application/Command/Member/CreateMemberFromSubscription/CreateMemberFromSubscriptionHandler.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Yoshi-Kan software.
+ *
+ * (c) Koen Caerels
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace App\YoshiKan\Application\Command\Member\CreateMemberFromSubscription;
 
 use App\YoshiKan\Domain\Model\Member\GradeRepository;
